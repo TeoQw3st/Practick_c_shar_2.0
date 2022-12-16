1bf0b090d09eb914e15636f9c57f46ddf43eed7d
Pavel <Qw3st@mail.ru>
2022-12-16 22:01:54 +0300

    f006

---
+++ FerstStep/FerstStep006_example_base/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 1;
+int b = 5;
+int c = 3;
+int d = 4;
+int e = 6;
+int max = a;
+if (a > max) max =a;
+if (b > max) max =b;
+if (c > max) max =c;
+if (d > max) max =d;
+if (e > max) max =e;
+Console.WriteLine(max);
+
