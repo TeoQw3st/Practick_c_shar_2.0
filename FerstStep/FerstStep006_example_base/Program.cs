9c222de0927c030fb8e253e9673b80e5f1c3d10b
Pavel <Qw3st@mail.ru>
2022-12-16 22:06:34 +0300

    f006

--- FerstStep/FerstStep006_example_base/Program.cs
+++ FerstStep/FerstStep006_example_base/Program.cs
@@ -1,5 +1,5 @@
 ﻿int a = 1;
-int b = 5;
+int b = 15;
 int c = 3;
 int d = 4;
 int e = 6;
@@ -9,5 +9,6 @@ if (b > max) max =b;
 if (c > max) max =c;
 if (d > max) max =d;
 if (e > max) max =e;
+Console.Write("Максимальное число равно ");
 Console.WriteLine(max);
 
