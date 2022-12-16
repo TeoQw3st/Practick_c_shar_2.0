d8d4bf75b604588b1e3369241c0317917ab6059f
Pavel <Qw3st@mail.ru>
2022-12-16 21:54:28 +0300

    F005

---
+++ FerstStep/FerstStep005_example_whileElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя польователя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура,этоже МАША!!!");
+}
+else
+{
+    Console.Write("привет,");
+    Console.WriteLine(username);
+}
